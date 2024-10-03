e956e869e6ead7ebc99808073823b261e173d3ce
Collins Okoroafor <collins.okoroafor@outlook.com>
2024-10-03 13:33:03 +0100

    updated connect.php.
    I added new user account details and uploaded the db and tb contents

--- templates/connect.php
+++ templates/connect.php
@@ -1,10 +1,14 @@
 <?php
 $hostname = "localhost";
+
 $username = "Lome";
 $password = "oseijoyalome2";
+$username_2 = "collins";
+$password_2 = "1234";
+
 $database = "recipes_db";
 
-$db_connect = mysqli_connect($hostname, $username, $password, $database);
+$db_connect = mysqli_connect($hostname, $username_2, $password_2, $database);
 if ($db_connect) {
 }else {
     echo "Failed" . mysqli_connect_error();
